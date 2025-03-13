f8198bc4af93c2bd4d252148d25a908f2d7007ed
BartCatalano <catalanobartolomeo@gmail.com>
2025-03-13 10:59:12 +0100

    feat: sistemato anche il ciclo for

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -16,7 +16,7 @@ public static void main(String[] args) {
     System.out.print("Inserisci il tuo nome ");
     String nomeInserito = in.nextLine();
     System.out.println("il nome inserito è " + nomeInserito);
-
+     in.close();
     boolean isInList = false;
     int i = 0;
     while( !isInList && i < arrayInvitati.length ){
@@ -32,3 +32,16 @@ if (isInList) { System.out.print("il nome inserito è nella lista ");
 } else{ System.out.print("il nome inserito non è nella lista");  }
 }
 }
+
+// per fare questo esercizio con il ciclo for possiamo utilizzare !isInList nella condizione del ciclo 
+
+// boolean isInList = false;
+// for (int i = 0 ; !isInList && i < arrayInvitati.length; i++ ){
+//     if(arrayInvitati[i].equals(nomeInserito)){
+//         isInList = true;
+//     }
+//     if (isInList) { System.out.print("il nome inserito è nella lista ");
+
+// } else{ System.out.print("il nome inserito non è nella lista");  }
+// }
+// }
